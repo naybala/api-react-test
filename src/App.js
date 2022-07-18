68a28805d6d97fb86013680bc5f8bbc87aa7ee04
naybala <naybalaismylife.nbl@gmail.com>
2022-07-18 14:02:38 +0630

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {Fragment , useEffect , useState} from "react";
 import List from "./Component/List";
+import Loader from "./Component/Loader";
 import "./App.css"
 function App() {
+  const [loader , setLoader] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoader(false);
+    },2000);
+  } )
+
   return (
-    <div className="App">
-      <List/>
-    </div>
+    <Fragment>
+       {loader ? 
+        <div className="App">
+          <Loader/>
+        </div> 
+     : 
+        <div className="App">
+          <List/>
+        </div>
+        }
+    </Fragment>
   );
 }
 export default App;
