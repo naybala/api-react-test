5296c8dfa6704a203423c0997ef536b9c6fe9372
naybala <naybalaismylife.nbl@gmail.com>
2022-07-19 12:28:06 +0630

    testing api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React, {Fragment , useEffect , useState} from "react";
+import React, {Fragment} from "react";
 import List from "./Component/List";
-import Loader from "./Component/Loader";
+
 import "./App.css"
 function App() {
-  const [loader , setLoader] = useState(true);
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      setLoader(false);
-    },2000);
-  } )
 
   return (
     <Fragment>
-       {loader ? 
-        <div className="App">
-          <Loader/>
-        </div> 
-     : 
-        <div className="App">
-          <List/>
-        </div>
-        }
+      <List/>
     </Fragment>
   );
 }
--- src/Component/List.jsx
+++ src/Component/List.jsx
@@ -1,42 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Fragment } from "react";
-
+import axios from "axios";
+import Loader from "./Loader";
 function List() {
+    const [data, setData] = useState([]);
+    const [loader, setLoader] = useState(true);
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/todos/").then((res) => {
+            const data = res.data;
+            setData(data);
+            setLoader(false);
+        })
+    })
+
     return (
         <Fragment>
-            <h1 className="text-center">List Is Here</h1>
-            <br /><br />
-            <div className="container bg-light rounded-3">
-                <div className="card border-0 rounded-2">
-                    <div className="card-header border-0">
-                        Name: Hello
-                    </div>
-                    <div className="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas
-                        <div className="mt-2">
-                            <span className="badge rounded-pill text-bg-primary">Success</span>
-                            <span className="badge rounded-pill text-bg-danger">Failure</span>
+            {loader ? <Loader />
+                :
+                <div>
+                    <h1 className="text-center">List Is Here</h1>
+                    <br /><br />
+                    <div className="container bg-light rounded-3">
+                        <div className="card border-0 rounded-2">
+                            <div className="card-header border-0">
+                                Name: Hello
+                            </div>
+                            <div className="card-body">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas
+                                <div className="mt-2">
+                                    <span className="badge rounded-pill text-bg-primary">Success</span>
+                                    <span className="badge rounded-pill text-bg-danger">Failure</span>
+                                </div>
+                            </div>
                         </div>
+                        <br />
                     </div>
+                    <br />
                 </div>
-                <br />
-            </div>
-            <br />
-            <div className="container bg-light rounded-3">
-                <div className="card border-0 rounded-2">
-                    <div className="card-header border-0">
-                        Name: Hello
-                    </div>
-                    <div className="card-body">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas
-                        <div className="mt-2">
-                            <span className="badge rounded-pill text-bg-primary">Success</span>
-                            <span className="badge rounded-pill text-bg-danger">Failure</span>
-                        </div>
-                    </div>
-                </div>
-                <br />
-            </div>
+            }
+
         </Fragment>
     )
 }
