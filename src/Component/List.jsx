48cf0fbb1a392ac86230462ae5c49ff091deae72
naybala <naybalaismylife.nbl@gmail.com>
2022-07-19 12:38:42 +0630

    get data done

--- src/Component/List.jsx
+++ src/Component/List.jsx
@@ -14,32 +14,38 @@ function List() {
     })
 
     return (
-        <Fragment>
+        <>
             {loader ? <Loader />
                 :
-                <div>
+                <Fragment>
                     <h1 className="text-center">List Is Here</h1>
                     <br /><br />
-                    <div className="container bg-light rounded-3">
-                        <div className="card border-0 rounded-2">
-                            <div className="card-header border-0">
-                                Name: Hello
-                            </div>
-                            <div className="card-body">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptas
-                                <div className="mt-2">
-                                    <span className="badge rounded-pill text-bg-primary">Success</span>
-                                    <span className="badge rounded-pill text-bg-danger">Failure</span>
+                    {
+                        data.map(d => (
+                            <div div className="container bg-light rounded-3" >
+                                <div className="card border-0 rounded-2">
+                                    <div className="card-header border-0">
+                                        Id: {d.id}
+                                    </div>
+                                    <div className="card-body">
+                                        {d.title}
+                                        <div className="mt-2">
+                                            {d.completed ?
+                                                <span className="badge rounded-pill text-bg-primary">Success</span>
+                                                :
+                                                <span className="badge rounded-pill text-bg-danger">Failure</span>}
+                                        </div>
+                                    </div>
                                 </div>
+                                <br />
                             </div>
-                        </div>
-                        <br />
-                    </div>
+                        ))
+                    }
                     <br />
-                </div>
+                </Fragment>
             }
+        </>
 
-        </Fragment>
     )
 }
 export default List;
